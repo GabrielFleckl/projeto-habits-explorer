a8b1169fa6b682500f88b57597c6c4bd41668202
Gabriel Leite <103828336+GabrielFleckl@users.noreply.github.com>
2023-01-20 18:01:15 -0300

    att

--- js/index.js
+++ js/index.js
@@ -1,16 +1,38 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('#btn')
 
-const data = {
-  run:['01-01', '01-02', '01-06'],
-  water: ['01-03'],
-  gym: [],
-  sleep: [],
-  study: []
+button.addEventListener('click', add)
 
+form.addEventListener('change', save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert('Dia já incluso')
+    return
+  }
+
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+function save() {
+  nlwSetup.data()
+}
+
+
+// const data = {
+//   run:['01-01', '01-02', '01-06'],
+//   water: ['01-03'],
+//   gym: [],
+//   sleep: [],
+//   study: []
+
+// }
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
 
 
